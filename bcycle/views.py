a25ba2387b364e1ee74ce4e6b55d82cab3d24165
Kyle Walters <kylewalters18@gmail.com>
2016-08-26 17:28:41 -0600

    trip endpoint and tests

--- bcycle/views.py
+++ bcycle/views.py
@@ -8,22 +8,31 @@ def serialize(datetime):
     return datetime.isoformat()
 
 
+def to_dict(trip):
+    trip_dict = dict(id=trip.id,
+                     bike_id=trip.bike_id,
+                     duration=trip.duration,
+                     checkout_kiosk=trip.checkout_kiosk,
+                     checkout_datetime=serialize(trip.checkout_datetime),
+                     return_kiosk=trip.return_kiosk,
+                     return_datetime=serialize(trip.return_datetime))
+    return trip_dict
+
+
 @app.route('/trip')
 def get_trips():
-    trips_result = [dict(bike_id=trip.bike_id,
-                         duration=trip.duration,
-                         checkout_kiosk=trip.checkout_kiosk,
-                         checkout_datetime=serialize(trip.checkout_datetime),
-                         return_kiosk=trip.return_kiosk,
-                         return_datetime=serialize(trip.return_datetime))
-                    for trip in Trip.query.all()]
-
+    trips_result = [to_dict(trip) for trip in Trip.query.all()]
     return json.dumps(trips_result)
 
 
 @app.route('/trip/<int:trip_id>')
 def get_trip(trip_id):
-    pass
+    trip_result = Trip.query.get(trip_id)
+    if trip_result:
+        trip = to_dict(trip_result)
+        return json.dumps(trip)
+    else:
+        return json.dumps({'error': 'no such trip'})
 
 
 @app.route('/rider')
@@ -34,6 +43,7 @@ def get_riders():
 @app.route('/rider/<int:rider_id>')
 def get_rider(rider_id):
     pass
+
 
 @app.errorhandler(404)
 def not_found(error):
--- tests/test_api.py
+++ tests/test_api.py
@@ -8,25 +8,31 @@ from unittest import mock
 from bcycle import app
 
 
+Trip = namedtuple('trip', ['id', 'bike_id', 'duration', 'checkout_kiosk',
+                           'checkout_datetime', 'return_kiosk', 'return_datetime'])
+
+
 class ApiTestCase(unittest.TestCase):
 
     def setUp(self):
         self.app = app.test_client()
 
+    def _verify_trip(self, actual, expected):
+        self.assertEqual(actual['id'], expected.id)
+        self.assertEqual(actual['bike_id'], expected.bike_id)
+        self.assertEqual(actual['duration'], expected.duration)
+        self.assertEqual(actual['checkout_kiosk'], expected.checkout_kiosk)
+        self.assertEqual(actual['checkout_datetime'], expected.checkout_datetime.isoformat())
+        self.assertEqual(actual['return_kiosk'], expected.return_kiosk)
+        self.assertEqual(actual['return_datetime'], expected.return_datetime.isoformat())
+
     def test_404_not_found(self):
         rv = self.app.get('/this_route_doesnt_exists')
-        self.assertIn(b'404 Not Found', rv.data)
+        self.assertEqual({"error": "not found"}, json.loads(rv.data.decode('UTF-8')))
 
     @mock.patch('bcycle.models.Trip.query')
-    def test_trip_endpoint(self, mock_query):
-        Trip = namedtuple('trip', ['bike_id',
-                                   'duration',
-                                   'checkout_kiosk',
-                                   'checkout_datetime',
-                                   'return_kiosk',
-                                   'return_datetime'])
-
-        trip = Trip(1, 30, "Main Street", datetime.now(), "1st Ave", datetime.now())
+    def test_get_trips_endpoint(self, mock_query):
+        trip = Trip(0, 1, 30, "Main Street", datetime.now(), "1st Ave", datetime.now())
         mock_query.all.return_value = [trip]
 
         rv = self.app.get('/trip')
@@ -34,10 +40,23 @@ class ApiTestCase(unittest.TestCase):
         self.assertEqual(len(response_data), 1)
 
         response_trip = response_data[0]
-        self.assertEqual(response_trip['bike_id'], trip.bike_id)
-        self.assertEqual(response_trip['duration'], trip.duration)
-        self.assertEqual(response_trip['checkout_kiosk'], trip.checkout_kiosk)
-        self.assertEqual(response_trip['checkout_datetime'], trip.checkout_datetime.isoformat())
-        self.assertEqual(response_trip['return_kiosk'], trip.return_kiosk)
-        self.assertEqual(response_trip['return_datetime'], trip.return_datetime.isoformat())
+        self._verify_trip(response_trip, trip)
 
+    @mock.patch('bcycle.models.Trip.query')
+    def test_no_trip_endpoint(self, mock_query):
+        mock_query.get.return_value = None
+
+        rv = self.app.get('/trip/0')
+        response_data = json.loads(rv.data.decode('UTF-8'))
+
+        self.assertEqual(response_data['error'], 'no such trip')
+
+    @mock.patch('bcycle.models.Trip.query')
+    def test_trip_endpoint(self, mock_query):
+        trip = Trip(0, 1, 30, "Main Street", datetime.now(), "1st Ave", datetime.now())
+
+        mock_query.get.return_value = trip
+        rv = self.app.get('/trip/0')
+        response_trip = json.loads(rv.data.decode('UTF-8'))
+
+        self._verify_trip(response_trip, trip)
