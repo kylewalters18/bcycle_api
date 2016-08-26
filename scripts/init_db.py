3931b161ff16acaa74c0baaa557427dc386ad646
Kyle Walters <kylewalters18@gmail.com>
2016-08-26 10:21:21 -0600

    added datetimes for trips

--- scripts/init_db.py
+++ scripts/init_db.py
@@ -1,19 +1,31 @@
 import csv
 
+from datetime import datetime
+
 from bcycle import db
 from bcycle.models import Rider, Trip
+
 
 Trip.query.delete()
 Rider.query.delete()
 
-with open('data/subset_2015denverbcycletripdata_public.csv', 'rb') as csvfile:
+
+def format_datetime(date, time):
+    as_string = '{} {}'.format(date, time)
+    date_time = datetime.strptime(as_string, '%m/%d/%y %I:%M:%S %p')
+    return date_time
+
+
+with open('data/subset_2015denverbcycletripdata_public.csv', 'r') as csvfile:
     riders = {}
     reader = csv.DictReader(csvfile, delimiter=',')
 
     for row in reader:
         trip = Trip(bike_id=row['bike'],
                     checkout_kiosk=row['checkout_kiosk'],
+                    checkout_datetime=format_datetime(row['checkout_date'], row['checkout_time']),
                     return_kiosk=row['return_kiosk'],
+                    return_datetime=format_datetime(row['return_date'], row['return_time']),
                     duration=row['duration_minutes'])
         db.session.add(trip)
 
