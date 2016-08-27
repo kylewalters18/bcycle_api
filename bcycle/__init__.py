39e5056c5f8b1af546cc5ead0408a8ca3719ad03
Kyle Walters <kylewalters18@gmail.com>
2016-08-27 11:51:42 -0600

    added cors support for client apps

--- bcycle/__init__.py
+++ bcycle/__init__.py
@@ -2,10 +2,11 @@ import os
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
+CORS(app)
 
 db = SQLAlchemy(app)
 
