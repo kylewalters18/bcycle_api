1ce5d3333edbe628a2a6f19bb4ffc06250eb2695
Kyle Walters <kylewalters18@gmail.com>
2016-10-06 22:02:20 -0600

    clean up formatting

--- bcycle/__init__.py
+++ bcycle/__init__.py
@@ -1,10 +1,12 @@
 import os
+
 from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
+
 CORS(app, origins=['*'])
 
 db = SQLAlchemy(app)
