4a9e91639d840f0dff2d16e9e5d73fd0d48e564e
Kyle Walters <kylewalters18@gmail.com>
2016-10-15 18:10:44 -0600

    experimenting with gzip optimization

--- bcycle/__init__.py
+++ bcycle/__init__.py
@@ -3,10 +3,14 @@ import os
 from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
+from flask_compress import Compress
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 
+# Ideally use Nginx for CORS and gzip compression
+# Not sure if possible with heroku deployment
+Compress(app)
 CORS(app, origins=['*'])
 
 db = SQLAlchemy(app)
